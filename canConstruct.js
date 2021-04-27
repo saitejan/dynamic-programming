e74f5863d964d1864d39e0701f48bc8e02b49611
Sai Teja <sai.teja@themathcompany.com>
2021-04-27 12:37:15 +0530

    construct a word from wordbank optimized using memoization

--- canConstruct.js
+++ canConstruct.js
@@ -1,17 +1,22 @@
-const canConstruct = (targetWord,wordBank) => {
+const canConstruct = (targetWord, wordBank, cache = {}) => {
+    if (targetWord in cache) return cache[targetWord]
     //checking current target
-    if(targetWord === "") return true;
+    if (targetWord === "") return true;
     //iterating over wordbank for all word combinations
-    for(let word of wordBank) {
+    for (let word of wordBank) {
         //checking whether target word starts with current word
-        if(targetWord.startsWith(word)) {
-            if(canConstruct(targetWord.slice(word.length),wordBank)) return true;
+        if (targetWord.startsWith(word)) {
+            if (canConstruct(targetWord.slice(word.length), wordBank,cache)) {
+                cache[targetWord] = true
+                return true;
+            }
         }
     }
+    cache[targetWord] = false
     return false
 }
 
 
-console.log(canConstruct('saiteja', ['sa','ja','teja','si','eja','i']))
-console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e','eee',
-'eeeeeeeeee','eeeeeeeee','eeeeeee','eeeeeeeeeeeeeeeeeeeeeeeeeee']))
+console.log(canConstruct('saiteja', ['sa', 'ja', 'teja', 'si', 'eja', 'i']))
+console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'eee',
+    'eeeeeeeeee', 'eeeeeeeee', 'eeeeeee', 'eeeeeeeeeeeeeeeeeeeeeeeeeee']))
